3409164178f577f490997d4539b5fc374dec1b84
Daniel <daniel.sims1994@outlook.com>
2018-05-09 20:56:03 +0100

    Fixed the activity theming to not hide the action bar

--- app/src/main/java/com/mylifeplannerco/mylifeplanner/ExerciseActivity.java
+++ app/src/main/java/com/mylifeplannerco/mylifeplanner/ExerciseActivity.java
@@ -2,7 +2,6 @@ package com.mylifeplannerco.mylifeplanner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class ExerciseActivity extends AppCompatActivity {
 
@@ -10,10 +9,5 @@ public class ExerciseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
